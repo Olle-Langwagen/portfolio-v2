2a3965d4e9b0a10b9d4b8cf8f894c188a68f0a5b
Olle-Langwagen <olle.langwagen@gmail.com>
2026-02-18 00:10:31 +0100

    + Project TypoTwist

--- lib/data.ts
+++ lib/data.ts
@@ -30,7 +30,7 @@ export const projects: Project[] = [
     link: "#",
     github: "https://github.com/Olle-Langwagen/portfolio-v2",
   },
-    {
+  {
     id: 3,
     title: "eCommerce webshop",
     description: "En e-commerce webshop byggd med vanilla JavaScript, HTML och CSS med fokus på användarupplevelse och responsiv design.",
@@ -49,7 +49,7 @@ export const projects: Project[] = [
     features: ["Docker API Integration", "Embedded Systems", "Fullstack IoT", "Real-time Monitoring", "Memory Optimization", "NTP Time Sync",  "Web Dashboard"],
     details: "Jag byggde detta projekt för att få en överblick över min hemmaservers containers utan att behöva logga in via SSH. \n\nSystemet består av två huvudkomponenter: \n\n1. En Backend (Python/Flask) som kommunicerar med Docker Socket för att hämta status på alla containers, sorterar containers med fel överst och exponerar datan via ett JSON-API.\n\n2. En ESP8266 + skärm som hämtar och visualiserar datan samt fungerar som webbserver. \n\nEn av de största tekniska utmaningarna var det begränsade RAM-minnet på ESP8266. För att förhindra krascher vid hantering av stora JSON-objekt och grafik samtidigt, implementerade jag 'Page Buffer'-rendering, via U8g2-biblioteket och dynamisk minnesallokering. Den byter sida automatiskt men man kan även klicka på flash knappen för att själv skrolla mellan sidorna. \n\nJag implementerade även bättre sortering av containers så att nere-liggande containers visas först, samt ett automatiskt nattläge som dimmar skärmen baserat på NTP-tidssynkronisering. \nJag gjorde även en webbpanel som körs direkt på ESPn för att enkelt se samma sak via en webbläsare."
   },
-    {
+  {
     id: 5,
     title: "Pokémon battle simulator",
     description: "Ett CLI-baserat program som simulerar den klassiska Pokémon-striden från Pokémon Red/Blue/Yellow, där användaren kan se striden utspela sig i terminalen.",
@@ -61,7 +61,7 @@ export const projects: Project[] = [
     details: "Jag byggde detta projekt som en rolig övning i Python, där jag ville återskapa den klassiska Pokémon-striden i en textbaserad form. \n\nProgrammet väljer tre slumpmässiga Pokémon åt både spelare och motståndare, och sedan simuleras striden i terminalen med färgkodad output för att visa skador, statusförändringar och typfördelar. \n\nJag använde Colorama-biblioteket för att göra output mer visuellt tilltalande, och Just_Playback för att lägga till ljud- och musikstöd under striden."
 
   },
-    {
+  {
     id: 6,
     title: "Trivia Game in Python",
     description: "Ett enkelt trivia-spel i Python gjort med customTkinter där användaren kan testa sina kunskaper inom olika kategorier och svårighetsgrader.",
@@ -72,7 +72,17 @@ export const projects: Project[] = [
     features: ["Multiple Categories", "Difficulty Levels", "Score Tracking", "CustomTkinter GUI", "CSV Data Storage"],
     details: "Jag byggde detta projekt som en övning i Python och GUI-utveckling med customTkinter. Spelet erbjuder flera kategorier (t.ex. Historia, Teknik, Musik) och tre svårighetsgrader (Lätt, Medel, Svår). \n\nAnvändaren kan svara eller passa på frågor, och poängen spåras under spelets gång, användaren kan sedan se sin slutpoäng när spelet är över. \n\nJag använde customTkinter för att skapa ett mer modernt och visuellt tilltalande GUI jämfört med standard Tkinter. \n\nDetta är ett gammalt projekt (~2021) men tycker ändå det är värt att visa upp :)"
   },
-
+  {
+    id: 7,
+    title: "TypoTwist",
+    description: "En hemsida för typography-nördar där användare kan skriva in en text och se den renderas i olika typsnitt och stilar. Simpel men elegant.",
+    tech: ["HTML", "CSS", "JavaScript", "Google Fonts API"],
+    link: "https://olle-langwagen.github.io/TypoTwist/",
+    github: "https://github.com/Olle-Langwagen/TypoTwist",
+    media: ["/TypoTwist.png"],
+    features: ["Font Preview", "Custom Text Input", "Google Fonts Integration"],
+    details: "Byggd med vanlig tech stack för enklast deployment på GitHub Pages. \n\nHemsidan visar hur en text kan renderas i olika typsnitt och stilar med hjälp av Google Fonts API. \n\nAnvändaren kan skriva in en text och se den renderas i realtid i olika typsnitt, vilket gör det till ett roligt verktyg för att utforska typografi och hitta det perfekta typsnittet för olika ändamål."
+  },
 ];
 
 
